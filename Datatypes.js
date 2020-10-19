98fecbdbb5f9d57221c6c5d2ec0d14dd21af56b5
ezike chris <christophermbe1009@gmail.com>
2020-10-19 10:32:59 +0100

    initial commit

---
+++ Datatypes.js
@@ -0,0 +1,79 @@
+// DATA TYPES IN JAVASCRIPT
+
+
+// Primitive Data Types
+
+// Stored directly in the location the variables accesses stored on the stack//
+
+// Six Primitive Data Types 
+
+// strings
+// sequencies of characters which can be letters numbers or symbols//
+//Number
+// integers, decimals numbers are all considered numbers in javascript//
+// Boolean
+// usually true or false
+//Null
+// an intentoinal empty value//
+// Undefined
+// variable which has not been assigned a value(all variable are undefined by default)
+//Symbols(ES6)
+
+
+// Reference  Data Types/ objects
+// Arrays
+//object literals
+//Function
+//Dates
+//Anything Else
+
+
+
+// Accessed by Referrence
+// Objects that are stored on the heap
+// Apointer to a location in memory
+
+
+//EXAMPLES/SAMPLES
+
+//PRIMITIVE
+
+//String
+const name = 'Chris Ezike'
+
+//Number
+const age = 45;
+// a number
+// placing quotes around the numbers would lead to it becoming a string
+
+//Boolean
+const hasKids = true;
+// placing quotes on booleans and they become a string
+
+//Null
+const car = Null;
+
+//Undefined
+let test;
+
+// symbol
+const sym = symbol();
+
+
+
+///Reference Types - Objects
+
+//Array
+const hobbies =[ 'movies', 'music'];
+//when logged becomes an object
+
+//Object Literal
+const address = {
+    city:'abuja',
+    state: 'F.C.T'
+}
+
+const today = new Date ();
+
+
+
---
+++ var,let,&const.js
@@ -0,0 +1,14 @@
+
+
+// define variable in javasrcipt(three main keywords)
+
+// var, let, & const(created using ES5-6 2015)
+
+// strings can be double quote or single qoute 
+
+var greeting;
+console.log(greeting);
+greeting = 'Hello';
+console.log(greeting);
+
+
